8908bc5e0ce79e28925428449dddc7696fdf58ae
NOT-ABOT <notabot1010@gmail.com>
2016-08-03 17:00:40 -0500

    Update and rename Depository to Depository.py

--- Depository.py
+++ Depository.py
@@ -2,3 +2,5 @@
 A depository file that will be hold all the comments already processed so that we are not replying to the
 same comments over and over again. This will result in getting banned.
 """
+
+matched = []
