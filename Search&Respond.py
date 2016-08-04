967e48a741599e9d1973926a84811ee0f153d37f
NOT-ABOT <notabot1010@gmail.com>
2016-08-04 13:41:56 -0500

    Update Search&Respond.py

--- Search&Respond.py
+++ Search&Respond.py
@@ -3,6 +3,12 @@
 ###########################################################################
 import re
 
+all_comment_types = [
+                    depression_words, 
+                    depression_responses, 
+                    curious_words,
+                    curous_responses,
+]
 depression_words = [
                     'I want to die',
                     'I want to kill myself',
@@ -10,5 +16,14 @@ depression_words = [
                     
 ] #This is what we are looking for. We will use regular expressions
 depression_response = [
-                      'Depression can hurt. If you need someone to talk to...'
+                      'Depression can hurt. If you need someone to talk to someone ' + 'I\'m here for you'
                       ] #This is what we are responding with. Examples here will be replaced with regular expressions
+curious_words = [
+                'I wonder'
+                'How does'
+                'What if'
+]
+
+curious_responses = [
+                    'Here, let me help you with that'
+]
