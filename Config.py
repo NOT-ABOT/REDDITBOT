28e8a4249b6b905dd7b650fd6c9b5a006ceab737
NOT-ABOT <notabot1010@gmail.com>
2016-08-04 11:11:29 -0500

    Created a configuration file

---
+++ Config.py
@@ -0,0 +1,7 @@
+"""
+useragent =
+app_id = Leave empty for now
+app_secret = leave empty for now
+Callback on redirect url: https://127.0.0.1:65010/authorize_callback
+r= praw.Reddit(useragent = '')
+"""
