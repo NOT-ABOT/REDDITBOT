ad60f40cf317e0385c152ffc8765003641ce43a2
NOT-ABOT <notabot1010@gmail.com>
2016-08-04 13:30:19 -0500

    Just a start

--- Search&Respond.py
+++ Search&Respond.py
@@ -3,5 +3,12 @@
 ###########################################################################
 import re
 
-words = [('I need help')] #This is what we are looking for. An example is listed
-response = [] #This is what we are responding with
+depression_words = [
+                    'I want to die',
+                    'I want to kill myself',
+                    'I hate life'
+                    
+] #This is what we are looking for. We will use regular expressions
+depression_response = [
+                      'Depression can hurt. If you need someone to talk to...'
+                      ] #This is what we are responding with. Examples here will be replaced with regular expressions
