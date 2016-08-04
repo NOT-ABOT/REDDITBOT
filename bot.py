dd91df27b97b14fdb9ae147e6936779769467d87
NOT-ABOT <notabot1010@gmail.com>
2016-08-04 09:15:29 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,18 +22,20 @@ response = "" #What are we gonna say?
 
 def action():
 	subreddit = r.get_subreddit('test')
-	comment = subreddit.get_comments(limit=75)
+	comments = subreddit.get_comments(limit=100)
 	for comment in comments:
-    	comment_text = comment.body.lower()
-    	author = comment.author.name
-    	match = any(string in comment_text for string in words)
-    	if str(comment.id) not in matched and match:
-    		if author.lower() != username.lower():
+		try:
+    		comment_text = comment.body.lower()
+    		author = comment.author.name
+    		match = any(string in comment_text for string in words)
+    		if str(comment.id) not in matched and match:
     			print("Found one by " + str(author))
       			comment.reply(response)
       			with open('depository.py', 'a') as myFile:
       				myFile.write(matched.append(str(comment.id)) + ', ')
       				myFile.close()
+      		except AttributeError:
+      				pass
       
 
 while True:
