061c71ce465ecca72edd862ec8aa68df7aa822b1
NOT-ABOT <notabot1010@gmail.com>
2016-08-04 10:31:41 -0500

    Create Search&Respond.py

---
+++ Search&Respond.py
@@ -0,0 +1,6 @@
+###########################################################################
+#       These will likely end up being dictionaries of dictionaries       #
+###########################################################################
+
+words = [] #This is what we are looking for
+response = [] #This is what we are responding with
