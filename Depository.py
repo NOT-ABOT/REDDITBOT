1c745fa110081b7715afa1dffb52c71e403834e5
NOT-ABOT <notabot1010@gmail.com>
2016-08-04 10:20:35 -0500

    Delete Depository.py

--- Depository.py
+++
@@ -1,6 +0,0 @@
-""" 
-A depository file that will be hold all the comments already processed so that we are not replying to the
-same comments over and over again. This will result in getting banned.
-"""
-
-matched = []
