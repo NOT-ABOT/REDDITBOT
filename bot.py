0dc5f3c176583a3ef234017fca0fce484a9c7ee1
NOT-ABOT <notabot1010@gmail.com>
2016-08-05 10:49:12 -0500

    Just trying something here

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import praw, time
 import sqlite3
 import re
-import oauth
+import admin,records
 url = 'http://www.google.com/?#q='
 username = 'TheHelpfulBot'
 depression_words = [
@@ -14,8 +14,8 @@ depression_words = [
                     'I am a failure'
                     'I don\'t deserve to live'
                     'I can\'t do this anymore'
-                    
-] 
+
+]
 depression_responses = [
                       'I\'m so sorry to hear that you\'re in pain. If you are in serious pain and need help' + ', ' + '[please visit this site](https://afsp.org/)',
                       'I understand your pain. [This site might be able to help](https://afsp.org/)'
@@ -37,53 +37,53 @@ relationship_words= [
 
 relationship_responses = [
                       'The most common way is to ask the person out'
-  
-  
+                      'Sometimes the most important thing is simply being there for someone'
+
+
 ]
 
 all_comment_types = [
-                    depression_words, 
+                    depression_words,
+					depression_responses,
                     curious_words,
+					curious_responses,
                     relationship_words,
+					relationship_responses
 ]
-all_comment_responses = [
-                      depression_responses,
-                      curious_responses,
-  
-]
+
 ###########################################################################################
-#Stil working in things, but I'm gonna try to test this thing out tonight, if possible    #
+#Still working in things, but I'm gonna try to test this thing out tonight, if possible    #
 ###########################################################################################
 
-print("Opening Database...")
-found = sqlite3.connect('answered.db') #create a database w/SQLite3 python library
-x = found.cursor()
-x.execute('CREATE TABLE IF NOT EXISTS answered(COMMENT TEXT, SUBMISSION TEXT)')
-found.commit()
+#r = praw.Reddit("A helpful friend with useful advice")
+#r.set_oauth_app_info(admin.app_id, admin.app_secret, admin.redirecturl)
+#r.get_authorize_url('...', oauth.app_scopes, True)
 
-print("Logging in to Reddit...")
-r = praw.Reddit("A helpful friend with useful advice")
-r.set_oauth_app_info(oauth.app_id, oauth.app_secret, oauth.redirecturl)
+def login():
+	r = praw.Reddit("A friend providing useful information and helpful advice")
+	r.set_oauth_app_info(admin.app_id, admin.app_secret, admin.redirecturl)
+	r.refresh_access_token(admin.refresh_token)
+	return r
+
 sub = 'test'
 maxposts = 100
 url = 'http://www.google.com/?#q='
+print('Logging in to Reddit...')
 
 class CommentReply:
-	
+
 	def __init__(self, comment_type, response_type):
 		self.comment_type = comment_type
 		self.response_type = response_type
-		
+
 	def reply_to_comment(comment_type, reponse_type):
 		comments = r.get_subreddit(sub).get_comments(limit=maxposts)
 		for comment in comments:
-			x.execute('Select * FROM answered WHERE COMMENT=?', [comment.id])
-			if not x.fetchone():
+			if comment.id not in answered_comments:
 				try:
 					author = comment.author.name
 					if author.lower() != username.lower():
 						comment_text = comment.body.lower()
-						string = comment.body.lower().split()
 						match = any(string.lower() in comment_text for word in comment_type)
 						if match:
 							print("Replying to " + author)
@@ -96,13 +96,13 @@ class CommentReply:
 				found.commit()
 
 
-      
+
 class SubmissionReply:
-		
+
 		def __init__(self, submission_type, submission_reply):
 			self.submission_type = submission_type
 			self.submission_reply = submission_reply
-			
+
 		def reply_to_submission(submission_type, _response_type):
 			submissions = r.get_subreddit(sub).get_new(limit=maxposts)
 			for submission in submissions:
@@ -120,8 +120,9 @@ class SubmissionReply:
 						pass
 					x.execute('INSERT INTO answered VALUES(?)', [submission.id])
 					found.commit()
-					
+
 
 while True:
-	CommentReply.reply_to_comment(depression_words, depression_responses)
-	time.sleep(5)
+	for i in range(len(all_comment_types)-1):
+        CommentReply.reply_to_comment(all_comment_types[i], all_coment_types[i+1])
+        time.sleep(5)
