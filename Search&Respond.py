e50272845530b50e99f737046ad833ecf4f67efd
NOT-ABOT <notabot1010@gmail.com>
2016-08-04 19:41:40 -0500

    Tinkering

--- Search&Respond.py
+++ Search&Respond.py
@@ -5,12 +5,15 @@ import re
 url = 'http://www.google.com/?#q='
 all_comment_types = [
                     depression_words, 
-                    depression_responses, 
                     curious_words,
-                    curous_responses,
                     relationship_words,
-                    relationship_responses
 ]
+all_comment_responses = [
+                      depressiong_responses,
+                      curious_responses,
+                      relationship_responses
+]
+
 depression_words = [
                     'I want to die',
                     'I want to kill myself',
@@ -26,7 +29,7 @@ depression_words = [
 #This is what we are looking for. We will use regular expressions later
 depression_response = [
                       'I\'m so sorry to hear that you\'re in pain. If you are in serious pain and need help' + ', ' + '[please visit this site](https://afsp.org/)',
-                      'I understand your pain. [This link might be able to help](https://afsp.org/)'
+                      'I understand your pain. [This site might be able to help](https://afsp.org/)'
 ] #This is what we are responding with. Examples here will be replaced with regular expressions
 curious_words = [
                 'I wonder why'
