3f2b355e15406c8624a36b932853fb72198c0967
NOT-ABOT <notabot1010@gmail.com>
2016-08-06 16:55:39 -0500

    This is the most current version

--- bot.py
+++ bot.py
@@ -5,25 +5,24 @@ import json, urllib
 
 url = 'http://www.google.com/?#q='
 username = 'TheHelpfulBot'
-disclaimer = ''
+disclaimer = '\n\n*I am a bot in beta mode. Please be patient with me as I figure things out.*'
 ##########################################################################################
 #Long responses section                                                                  #
 ##########################################################################################
-drep1 = '''
-I'm really sorry to hear that you feel this way. I'm gonna be straight up with you -- I'm a bot. But here's the thing:
-it doesn't take a human to see that you're special. Don't give up. Don't ever give up. And if you need someone to talk to,
-[feel free to message me at my human account](https://sf.reddit.com/user/___NOT_A_BOT___/). Hang in there, pal.
-'''
-drep2 = '''Hey there. Just wanted to say that I feel you. I know what it's like to be in pain. I remember when my parents died -- it was the worst
-feeling I had ever known. But here's the thing: no matter how hard it gets, giving up is not the answer. Stay strong and hang in there. Things will get better. '''
 
-deep1 = '''
-The question of God's existence has been around for a long time. Now, I'm just a bot, so I can't really as provide much insight as humans can, but
-here's my two cents: Has anyone ever been argued into believing in God? I don't think I've ever seen someone come to faith due to an intellectual argument.
-This is because religion, more than anything else, is emotional, psychological and spiritual, not intellectual. And on the flip side, attempting to argue one
-out of his/her religion is pointless for the simple reason that, if religion makes someone happy, trying to remove this source of happiness is cruel and counterproductive.
-I am, however, just a bot. And for that reason I must direct you to a [neutral source](https://en.wikipedia.org/wiki/Existence_of_God) of information regarding the topic
-at hand. I hope this helps! Have a nice day ;p '''
+f1 = '''
+I always wanted to be somebody, but now I realize I should have been more specific. - *Lily Tomlin* '''
+f2 = '''
+If at first you don’t succeed, then skydiving definitely isn’t for you. - *Steven Wright*'''
+f3= '''
+Luck is what you have left over after you give 100 percent. – *Langston Coleman*'''
+f4='''
+I didn’t fail the test. I just found 100 ways to do it wrong. – *Benjamin Franklin*'''
+f5='''
+The elevator to success is out of order. You’ll have to use the stairs… one step at a time. – *Joe Girard*'''
+f6='''
+It takes less time to do things right than to explain why you did it wrong. – *Henry Wadsworth Longfellow*'''
+d1 ='''Life: the condition that distinguishes animals and plants from inorganic matter, including the capacity for growth, reproduction, functional activity, and continual change preceding death.'''
 
 ################################################################################################################
 #Search list                                                                                                   #
@@ -37,35 +36,16 @@ params = {
 'key': 'apiKey',
 }
 
-god_words = [
-            'god(.*) exist(.*)'
+self_words = [
+            '/u/TheHelpfulBot'
 ]
 
-god_responses = [
-                deep1
-]
-depression_words = [
-                    'i(.*)(.*) (.*) depressed',
-                    'i(.*)(.*) a failure',
-                    'i(.*)(.*) to die',
-                    'kill(.*) myself',
-                    'i hate (.*) life',
-                    'i(.*)(.*) hav(.*) suicidal thoughts',
-                    'i don(.*)t have a(.*) reason to live',
-                    'i hate myself',
-                    'i don(.*)t deserve to live',
-                    'i can(.*)t take this (.*) anymore(.*)'
+self_responses = [f1,f2,f3,f4,f5,f6]
 
-]
-depression_responses = [
 
-                      'Life is filled with highs and lows. You may be at a low point right now, but stay strong! Things can always improve -- /u/XUDB',
-                      drep1,
-                      drep2
-]
 curious_words = [
-                'how do ',
-                'why does',
+                'how do(.*) ',
+                'why do(.*)',
                 'when is',
                 'how can i'
 ]
@@ -83,22 +63,12 @@ relationship_responses = [
                       'Sometimes the most important thing is simply being there for someone. This may help you get a date',
 ]
 
-silly_words = [
+deep_words = ['what(.*) (.*) the meaning of life(.*)'
 ]
 
-silly_responses = [
-]
-all_comment_types = [
-                    depression_words,
-					depression_responses,
-                    curious_words,
-					curious_responses,
-                    relationship_words,
-					relationship_responses,
-                    silly_words,
-                    silly_responses
-]
+deep_responses = [d1]
 
+dictionary_words =['What does (.*) mean?']
 ###########################################################################################################
 ###########################################################################################################
 
@@ -130,7 +100,8 @@ class CommentReply:
                         for i in range(len(comment_type)):
                             if re.match(comment_type[i], comment_text):
                                 print("Replying to /u/" + author)
-                                comment.reply(str(random.choice(response_type)) + disclaimer)
+                                r.send_message('___NOT_A_BOT___', 'Answered', 'Comment Answered')
+                                comment.reply(str(random.choice(response_type)))
                                 cur.execute('INSERT INTO oldposts VALUES(?)', [comment.id])
                                 sql.commit()
                             else:
@@ -147,43 +118,41 @@ class CommentReply:
                     author = comment.author.name
                     if author.lower() != username.lower():
                         comment_text = comment.body.lower()
-                        match = any(word.lower() in comment_text for word in curious_words)
-                        if match:
-                            print('Replying to /u/' + author)
-                            comment.reply(str(curious_responses[0] + comment_text + ') ' + disclaimer))
-                            cur.execute('INSERT INTO oldposts VALUES(?)', [comment.id])
-                            sql.commit()
+                        for i in range(len(curious_words)):
+                            if re.match(curious_words[i], comment_text):
+                                print('Replying to comment by /u/' + author + ' with url')
+                                comment.reply(str(curious_responses[0] + comment_text + ') ' + disclaimer))
+                                cur.execute('INSERT INTO oldposts VALUES(?)', [comment.id])
+                                sql.commit()
                 except AttributeError:
                     pass
 
 
-"""
-This is still not functional due to whitespace issues
 
 class SubmissionReply:
 
-		def __init__(self, submission_type, submission_reply):
-			self.submission_type = submission_type
-			self.submission_reply = submission_reply
+    def __init__(self, submission_type, submission_reply):
+        print('Parsing Submissions')
+        self.submission_type = submission_type
+        self.submission_reply = submission_reply
 
-		def reply_to_submission(submission_type, _response_type):
-			submissions = r.get_subreddit(sub).get_new(limit=maxposts)
-			for submission in submissions:
-				if submission.id not in answered_submissions:
-					try:
-						author = submission.author.name
-						if author.lower() != username.lower():
-							submission_text = submission.text.lower()
-							match = any(string in submission_text for word in submission_type)
-                            with open('records.py', 'a') as rec:
-                                answered_submissions.append(submission.id)
-                                rec.close()
-							if match:
-								print('Replying to ' + author)
-								submission.reply(random.choice(response_type))
-					except AtrributeError:
-						pass
-"""
+    def reply_to_submission(submission_type, _response_type):
+        submissions = r.get_subreddit(sub).get_new(limit=maxposts)
+        for submission in submissions:
+            if not cur.fetchone():
+                try:
+                    author = submission.author.name
+                    if author.lower() != username.lower():
+                        submission_text = submission.text.lower()
+                        for i in range(len(submission_type)):
+                            if re.match(submission_type[i], submission_text):
+                                print('Replying to submission by /u/' + author)
+                                submission.reply(random.choice(response_type))
+                                cur.execute('INSERT INTO oldposts VALUES(?)', [submission.id])
+                                sql.commit()
+                except AttributeError:
+                    pass
+
 
 def login():
     r = praw.Reddit('A helpful friend with useful advice')
@@ -193,11 +162,17 @@ def login():
 
 r = login()
 r
+num = 1
 while True:
-    print('Searching...')
-    CommentReply.reply_to_comment(depression_words, depression_responses)
+    print('Parsing comments...')
+    #CommentReply.reply_to_comment(depression_words, depression_responses)
     CommentReply.reply_to_comment(relationship_words, relationship_responses)
-    CommentReply.reply_to_comment(god_words, god_responses)
-    '''CommentReply.reply_to_comment(silly_words, silly_responses)
-    CommentReply.reply_to_comment_url()'''
+    #CommentReply.reply_to_comment(god_words, god_responses)
+    #SubmissionReply.reply_to_submission(depression_words, depression_responses)
+    CommentReply.reply_to_comment(self_words, self_responses)
+    CommentReply.reply_to_comment(deep_words, deep_responses)
+    #CommentReply.reply_to_comment_url()
     time.sleep(2)
+    num += 1
+    if num == 1200:
+        login()
